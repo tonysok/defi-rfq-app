e1f57c40a2f374947e4a632f9a16081037aaa083
Tony SOK <tony@flowdesk.co>
2024-07-14 00:30:25 +0200

    Update css

--- src/components/kyb/kyb.js
+++ src/components/kyb/kyb.js
@@ -1,10 +1,23 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import QRCode from 'react-qr-code'
+import './kyb.css'
+import styled from 'styled-components'
+
+const KybContainer = styled.div`
+    background-color: #fff;
+    padding: 40px;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    text-align: center;
+    max-width: 1200px;
+    width: 350px;
+    margin: 20px;
+`
 
 const Kyb = () => {
   const navigate = useNavigate()
-  let [qrCodeValue, setQrCodeValue] = React.useState('');
+  let [qrCodeValue, setQrCodeValue] = React.useState('')
   const payload = {
     'chainID': '80002',
     'skipClaimRevocationCheck': false,
@@ -27,43 +40,48 @@ const Kyb = () => {
       }
     ]
   }
-  useEffect(() => {
-    fetch('https://verifier-backend.polygonid.me/sign-in', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    })
-      .then(r => r.json())
-      .then((data) => {
-        const request_uri = data['qrCode'].split('iden3comm://?request_uri=')[1]
-        fetch(request_uri)
-          .then(r => r.json())
-          .then((response) => {
-            setQrCodeValue(JSON.stringify(response));
-            checkAuthenticationPeriodically(data['sessionID'])
-          })
-          .catch(err => console.log(err))
-      })
-      .catch(err => console.log(err))
+  fetch('https://verifier-backend.polygonid.me/sign-in', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
   })
+    .then(r => r.json())
+    .then((data) => {
+      const request_uri = data['qrCode'].split('iden3comm://?request_uri=')[1]
+      fetch(request_uri)
+        .then(r => r.json())
+        .then((response) => {
+          setQrCodeValue(JSON.stringify(response))
+          checkAuthenticationPeriodically(data['sessionID'])
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 
   function checkAuthenticationPeriodically (sessionId) {
     setInterval(() => {
       fetch('https://verifier-backend.polygonid.me/status?sessionID=' + sessionId)
         .then(r => r.json())
         .then((data) => {
-          if (data.status === 'success') {
-            navigate('/quotes');
+          switch (data.status) {
+            case 'success':
+              navigate('/quotes')
+              break
+            case 'pending':
+              break
+            default:
+              console.error(data)
+              navigate('/')
           }
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log(err))
     }, 1000)
   }
 
   return (
-    <div>
+    <KybContainer>
       <div id="qrcode">
         <QRCode
           size={100}
@@ -73,9 +91,9 @@ const Kyb = () => {
         />
       </div>
       <div className="instructions">
-        Please submit KYB.
+        Please submit your KYB
       </div>
-    </div>
+    </KybContainer>
   )
 }
 
